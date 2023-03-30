0c6769f9a80ece9c2615a2b6d8c1538cff0cbffc
Hossam Samir <hossamsamir@outlook.com>
2023-03-30 16:18:12 +0200

    shader class

--- include/shader_s.h
+++ include/shader_s.h
@@ -7,6 +7,11 @@
 #include <fstream>
 
 class Shader {
+public:
+    unsigned int ID;
+
+    // constructor generates the shader on the fly
+    // ------------------------------------------------------------------------
     Shader(const char *vertexPath, const char *fragmentPath) {
         // retrieve the vertex and fragment shader source code from filepath
         std::string vertexCode;
@@ -15,8 +20,8 @@ class Shader {
         std::ifstream fShaderFile;
 
         // ensure ifstream objects can throw exceptions:
-        vShaderFile.exceptions(std::ifstream::failbit || std::ifstream::badbit);
-        fShaderFile.exceptions(std::ifstream::failbit || std::ifstream::badbit);
+        vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
 
         try {
             vShaderFile.open(vertexPath);
@@ -41,8 +46,76 @@ class Shader {
         }
         const char *vShaderCode = vertexCode.c_str();
         const char *fShaderCode = vertexCode.c_str();
+
+
+        //2. compile shader
+        unsigned int vertex, fragment;
+        //vertex shader
+        // vertex shader
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, &vShaderCode, NULL);
+        glCompileShader(vertex);
+        checkCompileErrors(vertex, "VERTEX");
+        // fragment Shader
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, &fShaderCode, NULL);
+        glCompileShader(fragment);
+        checkCompileErrors(fragment, "FRAGMENT");
+        // shader Program
+        ID = glCreateProgram();
+        glAttachShader(ID, vertex);
+        glAttachShader(ID, fragment);
+        glLinkProgram(ID);
+        checkCompileErrors(ID, "PROGRAM");
+        // delete the shaders as they're linked into our program now and no longer necessary
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
     }
 
+    // activate the shader
+    // ------------------------------------------------------------------------
+    void use() {
+        glUseProgram(ID);
+    }
+
+    // utility uniform functions
+    // ------------------------------------------------------------------------
+    void setBool(const std::string &name, bool value) const {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), (int) value);
+    }
+
+    // ------------------------------------------------------------------------
+    void setInt(const std::string &name, int value) const {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+    }
+
+    // ------------------------------------------------------------------------
+    void setFloat(const std::string &name, float value) const {
+        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+    }
+
+private:
+    // utility function for checking shader compilation/linking errors.
+    // ------------------------------------------------------------------------
+    void checkCompileErrors(unsigned int shader, std::string type) {
+        int success;
+        char infoLog[1024];
+        if (type != "PROGRAM") {
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+            if (!success) {
+                glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+                std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog
+                          << "\n -- --------------------------------------------------- -- " << std::endl;
+            }
+        } else {
+            glGetProgramiv(shader, GL_LINK_STATUS, &success);
+            if (!success) {
+                glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+                std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog
+                          << "\n -- --------------------------------------------------- -- " << std::endl;
+            }
+        }
+    }
 };
 
 #endif //LEARN_OPENGL_SHADER_S_H
