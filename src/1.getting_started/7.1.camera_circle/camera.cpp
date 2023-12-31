7b34039338dfa96b3c7c24bc19de02ba9982d689
Hossam Samir <hossamsamir@outlook.com>
2023-12-31 20:12:42 +0200

    camera_circle example  -- textures and render loop

--- src/1.getting_started/7.1.camera_circle/camera.cpp
+++ src/1.getting_started/7.1.camera_circle/camera.cpp
@@ -11,6 +11,9 @@ const *char TITLE = "learn_opengl -- camera";
 
 void generate_cube_vertices(float **vertices, int &nverts);
 void copy_vertices_to_gpu(float **vertices, int nverts, uint &VAO, uint &VBO);
+
+void generate_texture(uint &texture, const char *img, bool flip);
+void render_loop(GLFWwindow *window, Shader &shader, uint VAO, uint texture1, uint texture2);
 int main()
 {
 	GLFWwindow *window = create_glfw_window(TITLE, SCR_WIDTH, SCR_HEIGHT);
@@ -98,4 +101,87 @@ void copy_vertices_to_gpu(float **vertices, int nverts, uint &VAO, uint &VBO)
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), I(void *)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 	glBindVertexArray(0);
+}
+
+void generate_texture(uint &texture, const char *img, bool flip)
+{
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	// set the texture wrapping parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_LINEAR);
+
+	// set texture filtering parameters
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// load image, create texture and generate mipmaps
+
+	stbi_set_flip_vertically_on_load(flip);
+	int width, height, nrChannels;
+
+	unsigned char *data = stbi_load(img, &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, (nrChannels == 4 ? GL_RGBA : GL_RGB), GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+}
+
+void render_loop(GLFWwindow *window, Shader &shader, uint VAO, uint texture1, uint texture2)
+{
+	while (!glfwWindowShouldClose(window))
+	{
+		/* code */
+
+		// process user inputs
+		processInput(window);
+		// clear background
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// activate textures-- bind textures on corresponding texture units
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+
+		// activate shader
+		//----------------------------------------------
+		shader.use();
+
+		// create transformation
+		//----------------------------------------------
+		glm::mat4 model = glm::mat4(1.0);
+		glm::mat4 view = glm::mat4(1.0);
+		glm::mat4 projection = glm::mat4(1.0);
+		model = glm::rotate(model, glm::radians(-55.f), glm::vec3(1.0, 0, 0));
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+
+		// retrieve the matrix uniform locations
+		unsigned int modelLoc = glGetUniformLocation(shader.ID, "model");
+		unsigned int viewLoc = glGetUniformLocation(shader.ID, "view");
+
+		// pass them to the shaders (3 different ways)
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+		// note: currently we set the projection matrix each frame, but since the projection
+		// matrix rarely changes it's often best practice to set it outside the main loop only once.
+		shader.setMat4("projection", projection);
+
+		// render container
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+		// -------------------------------------------------------------------------------
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 }
