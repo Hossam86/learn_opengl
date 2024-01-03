b411aa9f21b595e17aa78ae8e665cdc7bd536622
hossamali <hossamsamir@outlook.com>
2024-01-03 00:22:12 +0000

    cameraexample -- render loop

--- src/1.getting_started/7.1.camera_circle/camera.cpp
+++ src/1.getting_started/7.1.camera_circle/camera.cpp
@@ -69,6 +69,15 @@ int main() {
 	// -----------------------------------------------------------------------------------------------------------
 	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float) SCR_WIDTH / (float) SCR_HEIGHT, 0.1f, 100.0f);
 	shader.setMat4("projection", projection);
+
+	render_loop(window, shader, VAO, texture1, texture2);
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
+	delete[] vertices;
+
+
 }
 
 void generate_cube_vertices(float **vertices, int &nverts) {
@@ -182,27 +191,38 @@ void render_loop(GLFWwindow *window, Shader &shader, uint VAO, uint texture1, ui
 
 		// create transformation
 		//----------------------------------------------
-		glm::mat4 model = glm::mat4(1.0);
 		glm::mat4 view = glm::mat4(1.0);
-		glm::mat4 projection = glm::mat4(1.0);
-		model = glm::rotate(model, glm::radians(-55.f), glm::vec3(1.0, 0, 0));
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-		projection = glm::perspective(glm::radians(45.0f), (float) SCR_WIDTH / (float) SCR_HEIGHT, 0.1f, 100.0f);
+		float radius = 10.0f;
+		float camX = static_cast<float> (sin(glfwGetTime() * radius));
+		float camZ = static_cast<float> (cos(glfwGetTime() * radius));
+		view = glm::lookAt(glm::vec3(camX, 0.0f, camZ), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 
-		// retrieve the matrix uniform locations
-		unsigned int modelLoc = glGetUniformLocation(shader.ID, "model");
-		unsigned int viewLoc = glGetUniformLocation(shader.ID, "view");
-
-		// pass them to the shaders (3 different ways)
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
-		// note: currently we set the projection matrix each frame, but since the projection
-		// matrix rarely changes it's often best practice to set it outside the main loop only once.
-		shader.setMat4("projection", projection);
-
-		// render container
+		// retrieve the matrix uniform location and pass them to the shaders
+		shader.setMat4("view", view);
+		// render boxes
+		// world space positions of our cubes
+		glm::vec3 cubePositions[] = {
+				glm::vec3(0.0f, 0.0f, 0.0f),
+				glm::vec3(2.0f, 5.0f, -15.0f),
+				glm::vec3(-1.5f, -2.2f, -2.5f),
+				glm::vec3(-3.8f, -2.0f, -12.3f),
+				glm::vec3(2.4f, -0.4f, -3.5f),
+				glm::vec3(-1.7f, 3.0f, -7.5f),
+				glm::vec3(1.3f, -2.0f, -2.5f),
+				glm::vec3(1.5f, 2.0f, -2.5f),
+				glm::vec3(1.5f, 0.2f, -1.5f),
+				glm::vec3(-1.3f, 1.0f, -1.5f)
+		};
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		for (unsigned int i = 0; i < 10; i++) {
+			// calculate the model matrix for each object and pass it to shader before drawing
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			shader.setMat4("model", model);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
