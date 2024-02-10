89965e9d0b1787d2f6cfbe8792a6f1f2ebefb1b0
Hossam Samir <hossamsamir@outlook.com>
2024-02-10 16:45:30 +0200

    basic_lighting: diffuse term

---
+++ src/2.lighting/2.1.basic_lighting_diffuse/basic_lighting_diffuse.cpp
@@ -0,0 +1,18 @@
+
+#include "GLFW/glfw3.h"
+#include <cstddef>
+
+int
+main()
+{
+	glfwInit();
+	glfwInitHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwInitHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwInitHint(GLFW_OPENGL_CORE_PROFILE, true);
+
+#if __APPLE__
+	glfwInitHint(GLFW_OPENGL_FORWARD_COMPAT, true);
+#endif
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "learn_opengl:diffuse_light", NULL, NULL);
+}
