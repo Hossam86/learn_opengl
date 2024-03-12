b2f173df5f79c5e733a1701b9b71a3dafb0ab291
hossamali <hossamsamir@outlook.com>
2024-03-12 09:55:16 +0000

    frame time calc

--- src/8.guest/2024/8.occlusion_query/occlusion_query.cpp
+++ src/8.guest/2024/8.occlusion_query/occlusion_query.cpp
@@ -2,12 +2,20 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <learnopengl/camera.h>
+#include <learnopengl/shader.h>
 
 unsigned int SCR_WIDTH = 800;
 unsigned int SCR_HEIGHT = 600;
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 
 void
 processKeyboardInputs(GLFWwindow* window);
@@ -32,7 +40,7 @@ main()
 #if __APPLE__
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, , nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "occlusion query", nullptr, nullptr);
 
 	// register window callbacks
 	//--------------------------
@@ -93,8 +101,58 @@ main()
 	glBindVertexArray(cube2VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-	
-    // configure global opengl state
+
+	// configure global opengl state
 	// -----------------------------
-    
+	glEnable(GL_DEPTH_TEST);
+
+	// process shaders
+	Shader cubeShader("8.occlusion_tesh.vs", "8.occlusion_tesh.fs");
+
+	while (!glfwWindowShouldClose(window))
+	{
+		// per frame time logic
+		// ------------------------
+		float currentFrame = static_cast<float>(glfwGetTime());
+		deltaTime= currentFrame- lastFrame;
+		lastFrame= currentFrame;
+	}
+}
+
+void
+processKeyboardInputs(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+void
+mouseCursorCallback(GLFWwindow* window, double xposIn, double yposIn)
+{
+	float xpos = static_cast<float>(xposIn);
+	float ypos = static_cast<float>(yposIn);
+
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+	lastX = xpos;
+	lastY = ypos;
+
+	std::cout << "xoffset: " << xoffset << "\n";
+	camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+void
+framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
 }
