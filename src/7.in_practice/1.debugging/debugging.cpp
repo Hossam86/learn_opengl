03ad1688622de3a7ab360f50d63587fcfb27b84c
hossamali <hossamsamir@outlook.com>
2024-04-03 00:00:47 +0000

    glfw window creation

--- src/7.in_practice/1.debugging/debugging.cpp
+++ src/7.in_practice/1.debugging/debugging.cpp
@@ -1,3 +1,4 @@
+#include <cstddef>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
@@ -30,12 +31,12 @@ glCheckError_(const char* file, int line)
 		case GL_INVALID_OPERATION:
 			error = "INVALID_OPERATION";
 			break;
-		case GL_STACK_OVERFLOW:
-			error = "STACK_OVERFLOW";
-			break;
-		case GL_STACK_UNDERFLOW:
-			error = "STACK_UNDERFLOW";
-			break;
+		// case GL_STACK_OVERFLOW:
+		// 	error = "STACK_OVERFLOW";
+		// 	break;
+		// case GL_STACK_UNDERFLOW:
+		// 	error = "STACK_UNDERFLOW";
+		// break;
 		case GL_OUT_OF_MEMORY:
 			error = "OUT_OF_MEMORY";
 			break;
@@ -57,97 +58,147 @@ glCheckError_(const char* file, int line)
 
 // Requesting a debug context in GLFW is surprisingly easy as all we have to do is pass a hint to GLFW that we'd like to
 // have a debug output context. We have to do this before we call glfwCreateWindow:
-void APIENTRY
-glDebugOutput(
-	GLenum source,
-	GLenum type,
-	unsigned int id,
-	GLenum severity,
-	GLsizei length,
-	const char* message,
-	const void* userParam)
-{
-	// ignoew non-signficant error/warming codes
-	if (id == 131169 || id == 131185 || id == 131218 || id == 131204)
-		return;
-	std::cout << "-------------------" << std::endl;
-	std::cout << "Debug message (" << id << ")" << message << std::endl;
+// void APIENTRY
+// glDebugOutput(
+// 	GLenum source,
+// 	GLenum type,
+// 	unsigned int id,
+// 	GLenum severity,
+// 	GLsizei length,
+// 	const char* message,
+// 	const void* userParam)
+// {
+// 	// ignoew non-signficant error/warming codes
+// 	if (id == 131169 || id == 131185 || id == 131218 || id == 131204)
+// 		return;
+// 	std::cout << "-------------------" << std::endl;
+// 	std::cout << "Debug message (" << id << ")" << message << std::endl;
 
-	switch (source)
-	{
-	case GL_DEBUG_SOURCE_API:
-		std::cout << "Source: API";
-		break;
-	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
-		std::cout << "Source: Window System";
-		break;
-	case GL_DEBUG_SOURCE_SHADER_COMPILER:
-		std::cout << "Source: Shader Compiler";
-		break;
-	case GL_DEBUG_SOURCE_THIRD_PARTY:
-		std::cout << "Source: Third Party";
-		break;
-	case GL_DEBUG_SOURCE_APPLICATION:
-		std::cout << "Source: Application";
-		break;
-	case GL_DEBUG_SOURCE_OTHER:
-		std::cout << "Source: Other";
-		break;
-	}
-	std::cout << std::endl;
+// 	switch (source)
+// 	{
+// 	case GL_DEBUG_SOURCE_API:
+// 		std::cout << "Source: API";
+// 		break;
+// 	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+// 		std::cout << "Source: Window System";
+// 		break;
+// 	case GL_DEBUG_SOURCE_SHADER_COMPILER:
+// 		std::cout << "Source: Shader Compiler";
+// 		break;
+// 	case GL_DEBUG_SOURCE_THIRD_PARTY:
+// 		std::cout << "Source: Third Party";
+// 		break;
+// 	case GL_DEBUG_SOURCE_APPLICATION:
+// 		std::cout << "Source: Application";
+// 		break;
+// 	case GL_DEBUG_SOURCE_OTHER:
+// 		std::cout << "Source: Other";
+// 		break;
+// 	}
+// 	std::cout << std::endl;
 
-	switch (type)
-	{
-	case GL_DEBUG_TYPE_ERROR:
-		std::cout << "Type: Error";
-		break;
-	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
-		std::cout << "Type: Deprecated Behaviour";
-		break;
-	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
-		std::cout << "Type: Undefined Behaviour";
-		break;
-	case GL_DEBUG_TYPE_PORTABILITY:
-		std::cout << "Type: Portability";
-		break;
-	case GL_DEBUG_TYPE_PERFORMANCE:
-		std::cout << "Type: Performance";
-		break;
-	case GL_DEBUG_TYPE_MARKER:
-		std::cout << "Type: Marker";
-		break;
-	case GL_DEBUG_TYPE_PUSH_GROUP:
-		std::cout << "Type: Push Group";
-		break;
-	case GL_DEBUG_TYPE_POP_GROUP:
-		std::cout << "Type: Pop Group";
-		break;
-	case GL_DEBUG_TYPE_OTHER:
-		std::cout << "Type: Other";
-		break;
-	}
-	std::cout << std::endl;
+// 	switch (type)
+// 	{
+// 	case GL_DEBUG_TYPE_ERROR:
+// 		std::cout << "Type: Error";
+// 		break;
+// 	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+// 		std::cout << "Type: Deprecated Behaviour";
+// 		break;
+// 	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+// 		std::cout << "Type: Undefined Behaviour";
+// 		break;
+// 	case GL_DEBUG_TYPE_PORTABILITY:
+// 		std::cout << "Type: Portability";
+// 		break;
+// 	case GL_DEBUG_TYPE_PERFORMANCE:
+// 		std::cout << "Type: Performance";
+// 		break;
+// 	case GL_DEBUG_TYPE_MARKER:
+// 		std::cout << "Type: Marker";
+// 		break;
+// 	case GL_DEBUG_TYPE_PUSH_GROUP:
+// 		std::cout << "Type: Push Group";
+// 		break;
+// 	case GL_DEBUG_TYPE_POP_GROUP:
+// 		std::cout << "Type: Pop Group";
+// 		break;
+// 	case GL_DEBUG_TYPE_OTHER:
+// 		std::cout << "Type: Other";
+// 		break;
+// 	}
+// 	std::cout << std::endl;
 
-	switch (severity)
-	{
-	case GL_DEBUG_SEVERITY_HIGH:
-		std::cout << "Severity: high";
-		break;
-	case GL_DEBUG_SEVERITY_MEDIUM:
-		std::cout << "Severity: medium";
-		break;
-	case GL_DEBUG_SEVERITY_LOW:
-		std::cout << "Severity: low";
-		break;
-	case GL_DEBUG_SEVERITY_NOTIFICATION:
-		std::cout << "Severity: notification";
-		break;
-	}
-	std::cout << std::endl;
-	std::cout << std::endl;
-}
+// 	switch (severity)
+// 	{
+// 	case GL_DEBUG_SEVERITY_HIGH:
+// 		std::cout << "Severity: high";
+// 		break;
+// 	case GL_DEBUG_SEVERITY_MEDIUM:
+// 		std::cout << "Severity: medium";
+// 		break;
+// 	case GL_DEBUG_SEVERITY_LOW:
+// 		std::cout << "Severity: low";
+// 		break;
+// 	case GL_DEBUG_SEVERITY_NOTIFICATION:
+// 		std::cout << "Severity: notification";
+// 		break;
+// 	}
+// 	std::cout << std::endl;
+// 	std::cout << std::endl;
+// }
 
 int
 main()
 {
+	// glfw: initlize and configure
+	//------------------------------
+	glfwInit();
+	glfwInitHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwInitHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPACT, GL_TRUE);
+#endif
+
+	// glfw: window creation
+	//------------------------------
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDH, SCR_HEIGHT, "learn_opengl-debug ouput", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	if (window == NULL)
+	{
+		std::cout << "glfw: Failed to create window \n";
+	}
+
+	// call back functions
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// tell GLFW to capture our mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	while (glfwWindowShouldClose(window))
+	{
+	}
+}
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void
+process_input(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+
+void
+framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height)
 }
