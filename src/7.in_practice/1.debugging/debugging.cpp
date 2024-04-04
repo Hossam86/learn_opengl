f389e534d5ce042adc434dbe3049d84336305cec
hossamali <hossamsamir@outlook.com>
2024-04-04 04:40:15 +0000

    fix errors

--- src/7.in_practice/1.debugging/debugging.cpp
+++ src/7.in_practice/1.debugging/debugging.cpp
@@ -1,5 +1,12 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <learnopengl/shader.h>
+#include <stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <iostream>
 #include <string>
 
@@ -30,12 +37,12 @@ glCheckError_(const char* file, int line)
 		case GL_INVALID_OPERATION:
 			error = "INVALID_OPERATION";
 			break;
-		// case GL_STACK_OVERFLOW:
-		// 	error = "STACK_OVERFLOW";
-		// 	break;
-		// case GL_STACK_UNDERFLOW:
-		// 	error = "STACK_UNDERFLOW";
-		// break;
+		case GL_STACK_OVERFLOW:
+			error = "STACK_OVERFLOW";
+			break;
+		case GL_STACK_UNDERFLOW:
+			error = "STACK_UNDERFLOW";
+			break;
 		case GL_OUT_OF_MEMORY:
 			error = "OUT_OF_MEMORY";
 			break;
@@ -153,10 +160,10 @@ main()
 	// glfw: initlize and configure
 	//------------------------------
 	glfwInit();
-	glfwInitHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwInitHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true); // comment this line in a release build!
 
 #ifdef __APPLE__
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPACT, GL_TRUE);
@@ -168,7 +175,8 @@ main()
 	glfwMakeContextCurrent(window);
 	if (window == NULL)
 	{
-		std::cout << "glfw: Failed to create window \n";
+		std::cout << "glfw: Failed to create window! \n";
+		glfwTerminate();
 		return -1;
 	}
 
@@ -198,13 +206,277 @@ main()
 		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
 	}
 
-	// configure global opengl state;
-	//-----------------------------------
+	Shader shader("debugging.vs", "debugging.fs");
+	// configure 3D cube
+	float vertices[] = {
+		// back face
+		-0.5f,
+		-0.5f,
+		-0.5f,
+		0.0f,
+		0.0f, // bottom-left
+		0.5f,
+		0.5f,
+		-0.5f,
+		1.0f,
+		1.0f, // top-right
+		0.5f,
+		-0.5f,
+		-0.5f,
+		1.0f,
+		0.0f, // bottom-right
+		0.5f,
+		0.5f,
+		-0.5f,
+		1.0f,
+		1.0f, // top-right
+		-0.5f,
+		-0.5f,
+		-0.5f,
+		0.0f,
+		0.0f, // bottom-left
+		-0.5f,
+		0.5f,
+		-0.5f,
+		0.0f,
+		1.0f, // top-left
+			  // front face
+		-0.5f,
+		-0.5f,
+		0.5f,
+		0.0f,
+		0.0f, // bottom-left
+		0.5f,
+		-0.5f,
+		0.5f,
+		1.0f,
+		0.0f, // bottom-right
+		0.5f,
+		0.5f,
+		0.5f,
+		1.0f,
+		1.0f, // top-right
+		0.5f,
+		0.5f,
+		0.5f,
+		1.0f,
+		1.0f, // top-right
+		-0.5f,
+		0.5f,
+		0.5f,
+		0.0f,
+		1.0f, // top-left
+		-0.5f,
+		-0.5f,
+		0.5f,
+		0.0f,
+		0.0f, // bottom-left
+			  // left face
+		-0.5f,
+		0.5f,
+		0.5f,
+		-1.0f,
+		0.0f, // top-right
+		-0.5f,
+		0.5f,
+		-0.5f,
+		-1.0f,
+		1.0f, // top-left
+		-0.5f,
+		-0.5f,
+		-0.5f,
+		-0.0f,
+		1.0f, // bottom-left
+		-0.5f,
+		-0.5f,
+		-0.5f,
+		-0.0f,
+		1.0f, // bottom-left
+		-0.5f,
+		-0.5f,
+		0.5f,
+		-0.0f,
+		0.0f, // bottom-right
+		-0.5f,
+		0.5f,
+		0.5f,
+		-1.0f,
+		0.0f, // top-right
+		// right face
+		0.5f,
+		0.5f,
+		0.5f,
+		1.0f,
+		0.0f, // top-left
+		0.5f,
+		-0.5f,
+		-0.5f,
+		0.0f,
+		1.0f, // bottom-right
+		0.5f,
+		0.5f,
+		-0.5f,
+		1.0f,
+		1.0f, // top-right
+		0.5f,
+		-0.5f,
+		-0.5f,
+		0.0f,
+		1.0f, // bottom-right
+		0.5f,
+		0.5f,
+		0.5f,
+		1.0f,
+		0.0f, // top-left
+		0.5f,
+		-0.5f,
+		0.5f,
+		0.0f,
+		0.0f, // bottom-left
+			  // bottom face
+		-0.5f,
+		-0.5f,
+		-0.5f,
+		0.0f,
+		1.0f, // top-right
+		0.5f,
+		-0.5f,
+		-0.5f,
+		1.0f,
+		1.0f, // top-left
+		0.5f,
+		-0.5f,
+		0.5f,
+		1.0f,
+		0.0f, // bottom-left
+		0.5f,
+		-0.5f,
+		0.5f,
+		1.0f,
+		0.0f, // bottom-left
+		-0.5f,
+		-0.5f,
+		0.5f,
+		0.0f,
+		0.0f, // bottom-right
+		-0.5f,
+		-0.5f,
+		-0.5f,
+		0.0f,
+		1.0f, // top-right
+			  // top face
+		-0.5f,
+		0.5f,
+		-0.5f,
+		0.0f,
+		1.0f, // top-left
+		0.5f,
+		0.5f,
+		0.5f,
+		1.0f,
+		0.0f, // bottom-right
+		0.5f,
+		0.5f,
+		-0.5f,
+		1.0f,
+		1.0f, // top-right
+		0.5f,
+		0.5f,
+		0.5f,
+		1.0f,
+		0.0f, // bottom-right
+		-0.5f,
+		0.5f,
+		-0.5f,
+		0.0f,
+		1.0f, // top-left
+		-0.5f,
+		0.5f,
+		0.5f,
+		0.0f,
+		0.0f // bottom-left
+	};
+
+	// upload data to gpu
+	//---------------------------------
+	// Generate buffers
+	//----------------
+	unsigned int cubeVAO, cubeVBO;
+	glGenVertexArrays(1, &cubeVAO);
+	glGenBuffers(1, &cubeVBO);
+	// fill buffers
+	glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// link vertex attributes
+	//----------------------
+	glBindVertexArray(cubeVAO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+	// load cube texture
+	//------------------
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	int width, height, nrComponents;
+	unsigned char* data = stbi_load("../../resources/textures/wood.png", &width, &height, &nrComponents, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data); // fix 
+		// glTexImage2D(GL_FRAMEBUFFER, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	}
+	else
+	{
+		std::cout << "texture: Failed to load texture!" << std::endl;
+	}
+	stbi_image_free(data);
+
+	// setup projection matrix
+	//--------------------------
+	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDH / (float)SCR_HEIGHT, 0.1f, 10.0f);
+	shader.use(); // fix 
+	glUniformMatrix4fv(glGetUniformLocation(shader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+	glUniform1i(glGetUniformLocation(shader.ID, "tex"), 0);
+
+	// configure global opengl state
+	//-------------------------------
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
 
-	while (glfwWindowShouldClose(window))
+	// render loop
+	while (!glfwWindowShouldClose(window))
 	{
+		// input
+		//------
+		process_input(window);
+
+		// render
+		//------
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		shader.use();
+		float rotationSpeed = 10.0f;
+		float angle = (float)glfwGetTime() * rotationSpeed;
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.0, 0.0f, -2.5));
+		model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 1.0f, 1.0f));
+		glUniformMatrix4fv(glGetUniformLocation(shader.ID, "model"), 1, GL_FALSE, glm::value_ptr(model));
+
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glBindVertexArray(cubeVAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glBindVertexArray(0);
+
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
 		glfwSwapBuffers(window);
