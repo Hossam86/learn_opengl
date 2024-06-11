42772941878a3e0d0d96972ca52d009ff6d39638
Hossam Ali <hossamsamir@outlook.com>
2024-06-11 13:34:29 +0300

    call back funcs

--- src/4.advanced_opengl/6.1.cubemaps_skybox/cubemaps_skybox.cpp
+++ src/4.advanced_opengl/6.1.cubemaps_skybox/cubemaps_skybox.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
+#include <glad/glad.h>
+#include <learnopengl/shader_m.h>
+#include <learnopengl/model.h>
 
 const int SCR_WIDTH = 800;
 const int SCR_HEIGHT = 600;
 const char* TITLE = "learn_opengl--cube-maps";
+
+void
+process_input(GLFWwindow* window);
+void
+framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void
+mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void
+scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+unsigned int
+load_texture(const char* path);
+
+unsigned int
+load_cupemap(vector<std::string> faces);
 
 int
 main()
@@ -30,14 +48,59 @@ main()
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+	// register callbacks
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "glad: failed to load opengl functions\n";
+	}
 
 	while (!glfwWindowShouldClose(window))
 	{
+		// process inputs
+		process_input(window);
 
+		// render loop
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 	// optional: de-allocate all resources once they've outlived their purpose:
 	// ------------------------------------------------------------------------
 	glfwTerminate();
+}
+
+void
+process_input(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+void
+scroll_callback(GLFWwindow* window, double xoffest, double yoffset)
+{
+}
+
+void
+mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+}
+
+void
+framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+unsigned int
+load_texture(const char* path)
+{
+}
+
+unsigned int
+load_cubemap(vector<std::string> faces)
+{
 }
